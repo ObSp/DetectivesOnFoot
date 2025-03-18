4569aa81dd5fbde9e4b2769558a9bf1170866220
ObSp <150492054+ObSp@users.noreply.github.com>
2025-03-18 20:48:18 +0100

    a

--- backend/gameManager/gameManager.js
+++ backend/gameManager/gameManager.js
@@ -1,18 +1,88 @@
 class Game {
-    constructor() {
+    constructor(host) {
         this.players = [];
-        this.gameState = "waiting";
+        this.started = false;
+        this.host = host;
+        this.id = Math.floor(Math.random() * 10000);
+    }
+
+    start() {
+        this.started = true;
     }
 
     addPlayer(player) {
         this.players.push(player);
     }
 
-    startGame() {
-        this.gameState = "started";
+    removePlayer(player) {
+        this.players.splice(this.players.indexOf(player), 1);
     }
 
-    endGame() {
-        this.gameState = "ended";
+    getSerializedPlayers() {
+        return this.players.map(player => {
+            return {
+                name: player.name,
+                socketId: player.socketId,
+                isHost: player.name === this.host
+            }
+        });
     }
+
+    getPlayerFromName(name) {
+        return this.players.find(player => player.name == name);
+    }
+
+    getPlayerFromSocketId(socketId) {
+        return this.players.find(player => player.socketId === socketId);
+    }
+
+    emitToAllPlayers(event, ...data) {
+        this.players.forEach(player => player.socket.emit(event, ...data));
+    }
+}
+
+class Player {
+    constructor(name, socket) {
+        this.name = name;
+        this.socket = socket;
+        this.socketId = socket.id;
+    }
+
+    updateSocketId(socketId) {
+        this.socketId = socketId;
+    }   
+}
+
+let games = []
+
+function createGame(host) {
+    let game = new Game(host);
+    games.push(game);
+    return game;
+}
+
+function getGameFromId(id) {
+    return games.find(game => game.id == id);
+}
+
+function getGameFromHost(host) {
+    return games.find(game => game.host === host);
+}
+
+function getGameFromPlayer(player) {
+    return games.find(game => game.players.includes(player));
+}
+
+function getGameFromSocketId(socketId) {
+    return games.find(game => game.players.find(player => player.socketId === socketId));
+}
+
+module.exports = {
+    Game,
+    Player,
+    createGame,
+    getGameFromId,
+    getGameFromHost,
+    getGameFromPlayer,
+    getGameFromSocketId
 }
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const path = require("path")
+const cookie = require("cookie")
+const url = require("url")
 
 const app = require("express")()
 const server = require("http").createServer(app)
-const io = require("socket.io")(server, { cors: { origin: "*" }})
+const io = require("socket.io")(server,  {cors: {
+  origin: "https://example.com",
+  methods: ["GET", "POST"]
+}})
 const gameManager = require("./gameManager/gameManager")
 
 app.set("view engine", "ejs")
@@ -20,12 +25,27 @@ app.get("/host", (req, res) => {
   res.render("hostGame")
 })
 
-app.get("/join/:id", (req, res) => {
-  res.send("Joining game with id: " + req.params.id)
+app.get("/create", (req, res) => {
+  const game = gameManager.createGame(req.query.name)
+  res.redirect("/game?id=" + game.id + "&name=" + req.query.name)
 })
 
-app.get("/ingame", (req, res) => {
-  res.render("inGame")
+app.get("/game", (req, res) => {
+  const game = gameManager.getGameFromId(Number(req.query.id))
+  if (game && game.getPlayerFromName(req.query.name)) {
+    return res.redirect("/game?id=" + req.query.id + "&name=" + req.query.name + " (1)")
+  }
+  res.render("game", {playerName: req.query.name})
+})
+
+app.get("/get-game/:id", (req, res) => {
+  const game = gameManager.getGameFromId(req.params.id)
+
+  if (game) {
+    res.json(game)
+  } else {
+    res.status(404).json({error: "Game not found."})
+  }
 })
 
 const staticHandler = function(req, res, next) {
@@ -43,10 +63,37 @@ const handler404 = function(req, res) {
 app.use(staticHandler)
 app.use(handler404)
 
-app.listen(5000, () => {
+server.listen(5000, () => {
   console.log("Server is running on port 5000.")
 })
 
+console.log(gameManager.createGame().id)
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  const origUrl = (socket.request.headers.referer)
+  const params = new URLSearchParams(new URL(origUrl).search)
+  let playerName = params.get("name")
+  const gameId = params.get("id")
+  
+  const requestedGame = gameManager.getGameFromId(Number(gameId))
+
+  if (!requestedGame) {
+    return socket.emit("invalid_game_id")
+  }
+
+  const player = new gameManager.Player(playerName, socket)
+  requestedGame.addPlayer(player)
+  
+  if (requestedGame.host === playerName) {
+    //requestedGame.host = player
+  }
+
+  socket.emit("game_joined", requestedGame.id)
+
+  requestedGame.emitToAllPlayers("player_update", requestedGame.getSerializedPlayers())
+
+  socket.on("disconnect", () => {
+    requestedGame.removePlayer(player)
+    requestedGame.emitToAllPlayers("player_update", requestedGame.getSerializedPlayers())
+  })
 })
